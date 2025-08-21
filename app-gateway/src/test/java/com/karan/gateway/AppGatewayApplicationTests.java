4d9ba06b53f6d9baa8f53f85b35b2c82a0857d3b
Karan Rana <karan.rana@gecurrent.com>
2025-08-21 15:03:34 +0530

    more files related to api

---
+++ app-gateway/src/test/java/com/karan/gateway/AppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
